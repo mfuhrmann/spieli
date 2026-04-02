67097e8ceaf735014b2c98da3dcdcde385a37e3f
Ronny Trommer <ronny@no42.org>
2026-04-02 13:06:44 +0200

    Update config.js
    Configure the map for Landkreis Heilbronn

--- js/config.js
+++ js/config.js
@@ -1,13 +1,13 @@
 // OSM relation ID of the region to display.
 // Find it at https://www.openstreetmap.org/relation/<id> or by searching on https://nominatim.openstreetmap.org
-export const osmRelationId = 62700;
+export const osmRelationId = 62750;
 
 // Optional: shown in the "Daten ergänzen" modal.
 // Defaults to the generic OSM playground wiki page if not set.
 export const regionPlaygroundWikiUrl = 'https://wiki.openstreetmap.org/wiki/Fulda#Spielpl%C3%A4tze';
 
 // Optional: community chat link shown in the "Daten ergänzen" modal. Set to null to hide.
-export const regionChatUrl = 'https://matrix.to/#/#osm-fulda:matrix.org';
+export const regionChatUrl = 'TBD';
 
 // --- Map display settings (less commonly changed) ---
 export const mapZoom = 12;
